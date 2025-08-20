60f23e2752d1a166fb9b8f4fb5f075722d8b89d7
David Hui <dhui0226@gmail.com>
2025-08-19 20:10:54 -0700

    testing https

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -45,4 +45,8 @@ export class AppComponent implements OnInit {
   //   this.loggedIn = false;
   //   this.router.navigate(['/login'])
   // }
+
+  loginRedirect() {
+    window.location.href = 'https://127.0.0.1:8000/login'
+  }
 }
