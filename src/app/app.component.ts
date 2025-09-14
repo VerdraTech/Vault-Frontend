6c195b724c42faf2eea1b503ea00298886b92b33
David Hui <dhui0226@gmail.com>
2025-09-13 19:30:09 -0700

    scaffold pages for marketplace and dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,7 @@ import { Router } from '@angular/router';
 export class AppComponent implements OnInit {
   public appPages = [
     { title: 'Inbox', url: '/folder/inbox', icon: 'mail' },
+    { title: 'Dashboard', url: '/folder/dashboard', icon: 'analytics' },
     { title: 'Outbox', url: '/folder/outbox', icon: 'paper-plane' },
     { title: 'Inventory', url: '/folder/inventory', icon: 'archive' },
     { title: 'Favorites', url: '/folder/favorites', icon: 'heart' },
---
+++ src/app/pages/marketplace/marketplace.page.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+
+@Component({
+  selector: 'app-marketplace',
+  templateUrl: './marketplace.page.html',
+  styleUrls: ['./marketplace.page.scss'],
+  imports: [IonicModule]
+})
+export class MarketplacePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
